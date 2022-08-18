5986f6b8796dc068b2e82f922f7aa30c9ce332be
JhonatanFerrer <111335841+JhonatanFerrer@users.noreply.github.com>
2022-08-18 11:15:24 -0500

    Update sesion_est.php

--- sesion_est.php
+++ sesion_est.php
@@ -5,19 +5,27 @@ session_start();
 $cur=$_POST['curso'];
 $year=$_POST['anio'];
 $per=$_POST['periodo'];
+	$_SESSION['cur']=$cur;
+	$_SESSION['year']=$year;
+	$_SESSION['periodo']=$periodo;
 if ($per="Periodo I"){
 	$periodo="1";
 }
 else{
 	$periodo="2";
 }
-
-	$_SESSION['cur']=$cur;
-	$_SESSION['year']=$year;
-	$_SESSION['cur']=$cur;
-	$_SESSION['periodo']=$periodo;
+if ($cur="-- Seleccione una materia --"){
+	header("cursos.php");
+}
+else if ($year="-- Seleccione un año --"){
+	header("cursos.php");
+}
+else if ($per="-- Seleccione un periodo --"){
+	header("cursos.php");
+}
+else{
 	header("location: Estudiantes_Inscritos.php");
-
+}
 
 
 ?>
