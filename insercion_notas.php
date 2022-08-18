d79440d77168dd0ec8636ff1142eb49c3a0c4fc2
kyubichan <110320722+kyubichan@users.noreply.github.com>
2022-08-18 03:40:00 -0500

    Update insercion_notas.php

--- insercion_notas.php
+++ insercion_notas.php
@@ -16,6 +16,7 @@ $_SESSION['periodo']=$periodo;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagenes/logofox.ico" type="image/x-icon">
     <title>Document</title>
     <link rel="stylesheet" href="css/Insercion_notas.css">
 </head>
